5d3a54c9f362b5f9c51fb266634fba812cddef60
Rohmad DJ <rohmaddj@1337-2.local>
2019-12-06 16:55:20 +0700

    latest update

--- src/api/twelveType.js
+++ src/api/twelveType.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://twelvetypes.com/api"
-  baseURL: "http://127.0.0.1:8000/api"
+  baseURL: "https://api.individualogist.com/api"
+  //baseURL: "http://127.0.0.1:8000/api"
 });
