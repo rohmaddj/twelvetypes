c73e261ab44859403e724042ecbf9a078ba4cb5b
Rohmad DJ <rohmaddj@1337-2.local>
2019-12-06 16:53:46 +0700

    latest update before expired domain

--- src/components/pages/PersonalisedPage.js
+++ src/components/pages/PersonalisedPage.js
@@ -1,19 +1,20 @@
-import React from "react"
-import { connect } from "react-redux"
-import twelveTypes from "../../api/twelveType"
-import { Placeholder } from "semantic-ui-react"
-import Divider from "../child/Divider"
-import { addPersonalised } from "../../actions"
+import React from "react";
+import { connect } from "react-redux";
+import twelveTypes from "../../api/twelveType";
+import { Placeholder, Image, Container, Header } from "semantic-ui-react";
+import Divider from "../child/Divider";
+import { Link } from "react-router-dom";
+import { addPersonalised } from "../../actions";
 
 class PersonalisedPage extends React.Component {
-  state = { listArchetype: [], placeholder: true }
+  state = { listArchetype: [], placeholder: true };
 
   componentDidMount = () => {
     if (this.props.result.length > 0) {
-      let tmp = []
+      let tmp = [];
       this.props.result.map(data => {
-        tmp = [...tmp, Object.keys(data)[0]]
-      })
+        tmp = [...tmp, Object.keys(data)[0]];
+      });
       this.setState(
         {
           listArchetype: tmp
@@ -27,60 +28,46 @@ class PersonalisedPage extends React.Component {
               params: {
                 archetype: this.state.listArchetype
               }
-            })
-            this.props.addPersonalised(response.data.data)
+            });
+            this.props.addPersonalised(response.data.data);
             this.setState({
               placeholder: false
-            })
-            console.log(response)
+            });
+            console.log(response);
           } catch (error) {
-            console.log(error)
+            console.log(error);
           }
         }
-      )
+      );
     } else {
     }
-  }
+  };
 
   render() {
     const archetypes = this.props.personalised.map(archetype => {
       return (
-        <div key={archetype.id}>
-          <div className="row">
-            <div className="sixteen wide column custom-column-margin">
-              <h3 className="personalised-title">
-                {" "}
-                Your {archetype.archetype}
-              </h3>
-              <p>
-                The archetypes forming your personality comprises an incredibly
-                unique combination, representing a harmonious synergy between
-                your primary traits: "{archetype.p1}"
-              </p>
-            </div>
-
-            <div className="sixteen wide column">
-              <h3 className="ui header">Strengths:</h3>
-              <p>{archetype.p2}</p>
-            </div>
-
-            <div className="sixteen wide column">
-              <h3 className="ui header">Weaknesses:</h3>
-              <p>{archetype.p3}</p>
-            </div>
-          </div>
-
-          <div className="sixteen wide column">
+        <Container key={archetype.archetype}>
+          <Header as="h2" className={archetype.archetype}>
+            <Image
+              src={`https://individualogist.com/wp-content/themes/indivi-wp/img/icons/${archetype.archetype}.webp`}
+            />
+            <Header.Content>Your {archetype.archetype} Archetype</Header.Content>
+          </Header>
+          <div class="left-line">
+            <h3 className="ui header">Strengths:</h3>
+            <p>{archetype.p2}</p>
+            <h3 className="ui header">Weaknesses:</h3>
+            <p>{archetype.p3}</p>
             <h3 className="ui header">Goals:</h3>
             <p>{archetype.p4}</p>
           </div>
-        </div>
-      )
-    })
+        </Container>
+      );
+    });
 
     return (
       <div className="ui vertical stripe quote segment">
-        <Divider title="ARCHETYPE" />
+        <Divider title="ARCHETYPE RESULTS" />
         {this.state.placeholder ? (
           <div className="ui middle aligned stackable grid container">
             <div className="row">
@@ -120,16 +107,19 @@ class PersonalisedPage extends React.Component {
           <div className="ui middle aligned stackable grid container">
             <div className="row">
               <div className="sixteen wide column">
-                <h3 className="ui header">
-                  Hi, {this.props.username}, here is your personalised details:
-                </h3>
+                <h3 className="ui header">Hi {this.props.username}, here are your personalised results:</h3>
               </div>
             </div>
             {archetypes}
+            <div className="ui centered column row remove-padding">
+              <Link to="/archetype-details" className="ui huge submit button custom-background-orange">
+                Continue
+              </Link>
+            </div>
           </div>
         )}
       </div>
-    )
+    );
   }
 }
 
@@ -138,10 +128,10 @@ const mapStateToProps = state => {
     username: state.auth.username,
     result: state.quiz.result,
     personalised: state.quiz.personalised
-  }
-}
+  };
+};
 
 export default connect(
   mapStateToProps,
   { addPersonalised }
-)(PersonalisedPage)
+)(PersonalisedPage);
